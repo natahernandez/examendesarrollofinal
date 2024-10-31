e7fc34c227ae700baa336b717e5a9a00e256aeeb
natahernandez <ghernandeza13@miumg.edu.gt>
2024-10-31 15:30:00 -0600

    ajuste de seeder

--- seeders/20241031033633-seed-genres.js
+++ seeders/20241031033633-seed-genres.js
@@ -2,56 +2,60 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Genres', [
+    const genres = [
       {
         name: 'Ficción',
         description: 'Género que abarca narraciones imaginarias o inventadas.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'No Ficción',
         description: 'Género basado en hechos reales y personas reales.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Ciencia Ficción',
         description: 'Género que explora conceptos de ciencia y tecnología imaginarios.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Fantasía',
         description: 'Género que incluye elementos mágicos y mundos imaginarios.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Terror',
         description: 'Género que busca causar miedo en el lector.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Romance',
         description: 'Género centrado en relaciones amorosas.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Historia',
         description: 'Género basado en eventos históricos y personajes reales.',
-        createdAt: new Date(),
-        updatedAt: new Date()
       },
       {
         name: 'Misterio',
         description: 'Género que gira en torno a la resolución de un enigma o crimen.',
-        createdAt: new Date(),
-        updatedAt: new Date()
+      },
+    ];
+
+    for (const genre of genres) {
+      const [result] = await queryInterface.sequelize.query(
+        `SELECT * FROM Genres WHERE name = :name`,
+        { replacements: { name: genre.name } }
+      );
+
+      // Solo insertar si no existe
+      if (result.length === 0) {
+        await queryInterface.bulkInsert('Genres', [{
+          name: genre.name,
+          description: genre.description,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }]);
+        console.log(`Género '${genre.name}' creado exitosamente.`);
+      } else {
+        console.log(`El género '${genre.name}' ya existe, no se inserta.`);
       }
-    ]);
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
